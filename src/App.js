85c922da7ab0ff75135cc2cca6314bf7f953f98a
josealbertocortes <josealbertocortesayala94@gmail.com>
2022-02-08 12:49:33 -0600

    a

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+
 import './App.css';
+import { useForm } from './useForm/useForm';
 
 function App() {
+
+  const [error,setError]=useState(false)
+  const  [ {nombre,password}, handleInputChange ] =useForm({nombre:"",password:""})
+  const create= async (e)=>{
+    e.preventDefault()
+    let body={nombre,password}
+    try {
+      const res = await fetch("http://127.0.0.1:4200/user",{method:"POST",body:JSON.stringify(body)})
+      if(res.status===500) setError(true)
+    } catch (error) {
+   
+    }
+
+  }
+  const reintentar =()=>{
+    setError(!error)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {error?(<p>hubo un error <button onClick={reintentar}> reintentar</button> </p>):
+        <form onSubmit={create}>
+        <input  name="nombre" value={nombre} type="text" onChange={handleInputChange} />
+        <input  name="password" value={password} type="password" onChange={handleInputChange} />
+        <button type="submit">crear</button>
+      </form>
+        }
+       
       </header>
     </div>
   );
